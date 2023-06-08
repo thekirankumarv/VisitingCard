ee7b2e4aec821873f47567a3de99c69abff402e5
thekirankumarv <thekirankumarv@gmail.com>
2023-06-08 17:11:33 +0530

    initial commit

---
+++ app/src/main/java/com/example/card/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.card;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
